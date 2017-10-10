8b4527436f4883365b892a155084a18669c2f615
Melvin Musehani <info@appchemy.co.za>
2017-10-10 14:29:36 +0200

    Part 2B

---
+++ src/models/task.ts
@@ -0,0 +1,6 @@
+export interface Task {
+    id
+    description
+    state
+    createdAt
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,58 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController, ToastController } from 'ionic-angular';
+import {Task} from '../../models/task'
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  tasks: Array<Task> = []
+  state = 'todo'
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private alertController: AlertController, 
+    private toastController: ToastController) {
 
+  }
+
+  addTask() {
+    let alert = this.alertController.create({
+      title: 'New Task',
+      inputs: [
+        {
+          name: 'task',
+          placeholder: 'Task'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Save',
+          handler: data => {
+            this.saveTask(data['task'])
+          }
+        },
+        'Cancel'
+      ]
+    })
+
+    alert.present()
+  }
+
+  saveTask(task) {
+    let toastMessage = ''
+    if (task.trim().length == 0) {
+      toastMessage = 'You cannot create an empty task'
+    } else {
+      this.tasks.push(task)
+      toastMessage = 'Task added successfully'
+    }
+
+    let toast = this.toastController.create({
+      message: toastMessage,
+      duration: 3000
+    })
+
+    toast.present()
   }
 
 }
